ec2aa1e5a107f1170cbc3b600b3859615a03b20d
ryancasaguep <108260458+ryancasaguep@users.noreply.github.com>
2022-12-27 10:34:51 +0800

    Commit initial files

---
+++ main.js
@@ -0,0 +1,236 @@
+// let age;
+// age = 17 + 1;
+// let Name = "Ryan";
+//  Name = "Casaguep";
+// console.log(age);
+// console.log(Name);
+// document.getElementById("pota").innerHTML = "HEllow " + Name;
+// document.getElementsByClassName("bobo").console = "Good LUck";
+
+
+// let Num = 220;
+// let ans; 
+// ans = Num % 2 ;
+// console.log(ans);
+
+// const seven = 23;
+// const six = 24;
+// const answer = six + seven;
+// console.log(answer);
+
+// let username; 
+// document.getElementById("button").onclick = function(){
+//     username = document.getElementById("identity").value
+//     console.log(username)
+// }
+// /*
+// var ega = 21;
+// console.log("Age",ega);
+
+// const formula = 3.14;
+// let radius; 
+// radius = window.prompt("Enter Radius: ");
+// radius = radius * formula;
+// console.log(radius);
+
+// const formula1= 0.5;
+// let b ;
+// let h;
+// let area;
+// b = window.prompt("Enter Base: "); 
+// b = Number(b)
+// h = window.prompt("Enter Height: "); 
+// h = Number(h)
+// area = (formula) * b * h;
+// console.log("Area: ",area);
+// area = Math.sqrt(area);
+// console.log("Sqrt: ",area);
+
+// */
+
+// document.getElementById("sub-button").onclick = function(){
+//     a = document.getElementById("sub1").value;
+//     a = Number(a);
+//     b = document.getElementById("sub2").value;
+//     b = Number(b);
+//     c = document.getElementById("sub3").value;
+//     c = Number(c);
+
+//     grade = (a + b + c) /3;
+//     document.getElementById("ave").innerHTML = "Average: " + grade;
+
+// }
+
+// document.getElementById("add").onclick = function(){
+//     num1 = document.getElementById("num1").value;
+//     num1 = Number(num1);
+//     num2 = document.getElementById("num2").value;
+//     num2 = Number(num2);
+    
+
+//     wet = num1 + num2;
+//     document.getElementById("result").innerHTML = "Result: " + wet;
+
+// }
+
+// document.getElementById("sub").onclick = function(){
+//     num1 = document.getElementById("num1").value;
+//     num1 = Number(num1);
+//     num2 = document.getElementById("num2").value;
+//     num2 = Number(num2);
+    
+
+//     wet = num1 - num2;
+//     document.getElementById("result").innerHTML = "Result: " + wet;
+
+// }
+
+// document.getElementById("multi").onclick = function(){
+//     num1 = document.getElementById("num1").value;
+//     num1 = Number(num1);
+//     num2 = document.getElementById("num2").value;
+//     num2 = Number(num2);
+    
+
+//     wet = num1 * num2;
+//     document.getElementById("result").innerHTML = "Result: " + wet;
+
+// }
+
+// document.getElementById("div").onclick = function(){
+//     num1 = document.getElementById("num1").value;
+//     num1 = Number(num1);
+//     num2 = document.getElementById("num2").value;
+//     num2 = Number(num2);
+    
+
+//     wet = num1 / num2;
+//     document.getElementById("result").innerHTML = "Result: " + wet;
+
+// }
+// document.getElementById("reset").onclick = function(){
+//     reset = document.getElementById("result").value;
+//     reset=0;
+//     document.getElementById("result").innerHTML = "Result: " +reset; 
+// }
+
+
+
+// let count = 0;
+// document.getElementById("inc").onclick = function (){
+//     count = count + 1;
+//     document.getElementById("count").innerHTML = count;
+// }
+// document.getElementById("dec").onclick = function (){
+//     count = count - 1;
+//     document.getElementById("count").innerHTML = count;
+// }
+// document.getElementById("res").onclick = function (){
+//     count = 0;
+//     document.getElementById("count").innerHTML = count;
+// }
+
+
+// let FullName = "Ryan Casaguep";
+// let FirstName;
+// let Lastname;
+
+// FirstName = FullName.slice(0, FullName.indexOf (" "));
+// console.log(FirstName);
+// Lastname = FullName.slice(FullName.indexOf (" ") +1);
+// console.log(Lastname);
+
+// let message = "Welcome Back, "
+// let yourname = "Ryan"
+// let welcomeEL = message + yourname;
+// document.getElementById("welcome").innerHTML = welcomeEL;
+
+
+// let label;
+// document.getElementById("myBtn").onclick = function (){
+//     label = document.getElementById("myInput").value
+//     console.log(label)
+// }
+
+    // let label;
+    // function myFunc(){
+    //     label = document.getElementById("myInput").value
+    //     console.log(label)
+    // }
+ 
+
+
+    // let Count = 0;
+    // function Inc(){
+    //     Count += 1;
+    //     document.getElementById("count").innerText = Count;
+    // }
+
+    // let entry ;
+    // function Save(){
+    //     let entry = Count + " - " ;
+    //     document.getElementById("save").textContent += entry
+    //     document.getElementById("count").textContent = 0;
+    //     Count = 0
+    // }
+
+    function add(){
+        
+    }
+
+    let by = 0;
+    let py = 2022
+
+    const yourAge = py - by;
+
+    if (yourAge >= 18) {
+        console.log("You are " + yourAge + " years old.")
+        console.log("You are Legal Age")
+     }
+     else if (yourAge <= 17){
+        console.log("You are " + yourAge + " years old.")
+        console.log("You are Minor Age")
+     }
+     else{
+        console.log("Error")
+     } 
+
+
+             //SCRIMBA SCORING BOARD
+
+   let pointsH = 0;
+   let pointsG = 0; 
+    function add1h(){
+         pointsH += 1;
+         document.getElementById("score-h").innerText = pointsH;
+    }
+
+    function add2h(){
+      pointsH += 2;
+      document.getElementById("score-h").innerText = pointsH;
+    }
+    function add3h(){
+      pointsH += 3;
+      document.getElementById("score-h").innerText = pointsH;
+    }
+
+    function add1g(){
+      pointsG += 1;
+      document.getElementById("score-g").innerText = pointsG;
+    }
+    function add2g(){
+      pointsG += 2;
+      document.getElementById("score-g").innerText = pointsG;
+    }
+    function add3g(){
+      pointsG += 3;
+      document.getElementById("score-g").innerText = pointsG;
+    }
+
+
+
+
+
+
+ 
+
